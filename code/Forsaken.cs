37418c0a110e77000c8540dcfb7ecc2a5ba50a1b
kurozael <kurozael@gmail.com>
2022-11-01 14:43:39 +0000

    Added a basic Hud class and instantiate it in the Forsaken game class

--- code/Forsaken.cs
+++ code/Forsaken.cs
@@ -6,13 +6,26 @@ namespace Facepunch.Forsaken;
 
 public partial class Forsaken : Game
 {
+	public Forsaken()
+	{
+		if ( IsClient )
+		{
+			Local.Hud?.Delete( true );
+			Local.Hud = new Hud();
+		}
+	}
+
 	public override void ClientJoined( Client client )
 	{
 		base.ClientJoined( client );
 
 		var pawn = new Player();
 		client.Pawn = pawn;
+		pawn.Respawn();
+	}
 
+	public override void MoveToSpawnpoint( Entity pawn )
+	{
 		var spawnpoints = All.OfType<SpawnPoint>();
 		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
 
