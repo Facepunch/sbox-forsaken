24f70d685da324e02bc8d3c5db21488e0f58ab7f
kurozael <kurozael@gmail.com>
2022-11-01 14:44:02 +0000

    Added a simple Top Down Camera class

---
+++ code/player/TopDownCamera.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sandbox;
+
+namespace Facepunch.Forsaken;
+
+public partial class TopDownCamera : CameraMode
+{
+	public float Height { get; set; } = 450f;
+	public float MoveSpeed { get; set; } = 20f;
+
+	public override void Update()
+	{
+		if ( Local.Pawn is not Player pawn )
+			return;
+
+		var targetPosition = pawn.Position.WithZ( pawn.Position.z + Height );
+
+		Position = Position.LerpTo( targetPosition, Time.Delta * MoveSpeed );
+		Rotation = Rotation.LookAt( Vector3.Down );
+		FieldOfView = 70f;
+		Viewer = null;
+	}
+}
