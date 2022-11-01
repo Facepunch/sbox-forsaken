ea919860c31debe6beeeffc268a5e1b279a12b5a
kurozael <kurozael@gmail.com>
2022-11-01 02:03:31 +0000

    Added initial .addon setup and basic Forsaken game class

---
+++ code/Forsaken.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace Facepunch.Forsaken;
+
+public partial class Forsaken : Game
+{
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		var pawn = new Player();
+		client.Pawn = pawn;
+
+		var spawnpoints = All.OfType<SpawnPoint>();
+		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+
+		if ( randomSpawnPoint != null )
+		{
+			var tx = randomSpawnPoint.Transform;
+			tx.Position = tx.Position + Vector3.Up * 50f;
+			pawn.Transform = tx;
+		}
+	}
+}
