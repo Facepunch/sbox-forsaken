1926e97db147c9f128745b4ff63591972f569db7
kurozael <kurozael@gmail.com>
2022-11-08 10:42:49 +0000

    Add MP5A4 and Crossbow game resources

--- code/items/weapons/CrossbowItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Facepunch.Forsaken;
-
-public class CrossbowItem : WeaponItem
-{
-	public override string WeaponName => "weapon_crossbow";
-	public override string Description => "A light damage crossbow for dealing ranged damage.";
-	public override string Icon => "textures/items/weapon_crossbow.png";
-	public override string Name => "Crossbow";
-}
