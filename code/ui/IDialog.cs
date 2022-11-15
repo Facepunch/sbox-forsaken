01211ed3d7f9a11c239c0d0631ef88aac4afb0bc
kurozael <kurozael@gmail.com>
2022-11-15 11:13:49 +0000

    Add Dialog separate to IDialog

--- code/ui/IDialog.cs
+++ code/ui/IDialog.cs
@@ -1,47 +1,7 @@
-﻿using Sandbox;
-
-namespace Facepunch.Forsaken.UI;
+﻿namespace Facepunch.Forsaken.UI;
 
 public interface IDialog
 {
-	public static IDialog Active { get; private set; }
-
-	public static void Activate( IDialog dialog )
-	{
-		if ( Active != dialog )
-		{
-			Active = dialog;
-		}
-	}
-
-	public static void Deactivate( IDialog dialog )
-	{
-		if ( Active == dialog )
-		{
-			Active = null;
-		}
-	}
-
-	public static bool IsActive()
-	{
-		return Active?.IsOpen ?? false;
-	}
-
-	public static void CloseActive()
-	{
-		Active?.Close();
-	}
-
-	[Event.BuildInput]
-	private static void BuildInput()
-	{
-		if ( Active?.IsOpen ?? false )
-		{
-			Input.StopProcessing = true;
-			Input.AnalogMove = Vector3.Zero;
-		}
-	}
-
 	bool IsOpen { get; }
 	void Open();
 	void Close();
