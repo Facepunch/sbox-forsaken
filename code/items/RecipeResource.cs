529d412701bd9b24b07cfed0c2cdca0aaf683ac8
kurozael <kurozael@gmail.com>
2022-11-17 15:11:13 +0000

    Added RecipeResource (game resource)

---
+++ code/items/RecipeResource.cs
@@ -0,0 +1,11 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace Facepunch.Forsaken;
+
+[GameResource( "Recipe", "recipe", "A crafting recipe to produce an item in Forsaken.", Icon = "auto_awesome" )]
+public class RecipeResource : GameResource
+{
+	[Property] public string Output { get; set; }
+	[Property] public Dictionary<string,int> Inputs { get; set; }
+}
