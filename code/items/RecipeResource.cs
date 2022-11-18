d965fe3747021f4c2f41c77c270bc8e39369df09
kurozael <kurozael@gmail.com>
2022-11-18 04:33:51 +0000

    Added StackSize and some documentation to RecipeResource

--- code/items/RecipeResource.cs
+++ code/items/RecipeResource.cs
@@ -6,6 +6,18 @@ namespace Facepunch.Forsaken;
 [GameResource( "Recipe", "recipe", "A crafting recipe to produce an item in Forsaken.", Icon = "auto_awesome" )]
 public class RecipeResource : GameResource
 {
+	/// <summary>
+	/// The unique id of the item to produce as an output.
+	/// </summary>
 	[Property] public string Output { get; set; }
+
+	/// <summary>
+	/// The producted quantity of the output item.
+	/// </summary>
+	[Property] public int StackSize { get; set; } = 1;
+
+	/// <summary>
+	/// The unique item ids and their quantities required to produce the output item.
+	/// </summary>
 	[Property] public Dictionary<string,int> Inputs { get; set; }
 }
