6e3c6319cc3dbe5e173762ad588617977af65233
kurozael <kurozael@gmail.com>
2022-11-18 06:23:59 +0000

    Use Color cast for hex colors in ItemColors class

--- code/items/ItemColors.cs
+++ code/items/ItemColors.cs
@@ -2,11 +2,11 @@
 
 public static class ItemColors
 {
-	public static Color Weapon { get; private set; } = new Color( 169 / 255f, 73 / 255f, 73 / 255f );
-	public static Color Armor { get; private set; } = new Color( 92 / 255f, 105 / 255f, 159 / 255f );
-	public static Color Consumable { get; private set; } = new Color( 232 / 255f, 198 / 255f, 91 / 255f );
-	public static Color Ammo { get; private set; } = new Color( 139 / 255f, 145 / 255f, 80 / 255f );
-	public static Color Material { get; private set; } = new Color( 139 / 255f, 176 / 255f, 173 / 255f );
-	public static Color Deployable { get; private set; } = new Color( 200 / 255f, 150 / 255f, 173 / 255f );
-	public static Color Tool { get; private set; } = new Color( 139 / 255f, 176 / 255f, 173 / 255f );
+	public static Color Weapon { get; private set; } = (Color)"#a94949";
+	public static Color Armor { get; private set; } = (Color)"#5c699f";
+	public static Color Consumable { get; private set; } = (Color)"#e8c75b";
+	public static Color Ammo { get; private set; } = (Color)"#8b9150";
+	public static Color Material { get; private set; } = (Color)"#8bb0ad";
+	public static Color Deployable { get; private set; } = (Color)"#c896ad";
+	public static Color Tool { get; private set; } = (Color)"#8bb0ad";
 }
