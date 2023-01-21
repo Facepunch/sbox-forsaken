ae1da3eb90651d63bd0d647d4a9fc758ef4c7d42
kurozael <kurozael@gmail.com>
2023-01-21 21:09:48 +0000

    Persistence++

--- code/persistence/PersistenceSystem.cs
+++ code/persistence/PersistenceSystem.cs
@@ -9,7 +9,7 @@ namespace Facepunch.Forsaken;
 
 public static class PersistenceSystem
 {
-	public static int Version => 12;
+	public static int Version => 13;
 
 	private static Dictionary<long, byte[]> PlayerData { get; set; } = new();
 	private static ulong PersistentId { get; set; }
