14202b1fe04f826bd269e3efe0b3b7be22393479
kurozael <kurozael@gmail.com>
2023-01-21 21:10:32 +0000

    Increase crate respawn time to 180

--- code/entities/hammer/MilitaryCrate.cs
+++ code/entities/hammer/MilitaryCrate.cs
@@ -8,7 +8,7 @@ namespace Facepunch.Forsaken;
 public partial class MilitaryCrate : LootSpawner
 {
 	public override string Title { get; set; } = "Military Crate";
-	public override float RestockTime { get; set; } = 30f;
+	public override float RestockTime { get; set; } = 180f;
 	public override int SlotLimit { get; set; } = 6;
 	public override float MinLootChance { get; set; } = 0f;
 	public override float MaxLootChance { get; set; } = 0.5f;
