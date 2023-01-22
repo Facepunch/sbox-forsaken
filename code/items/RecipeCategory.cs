d9ec2f854707477b878817d8d6f292ddb32df82f
kurozael <kurozael@gmail.com>
2023-01-22 19:40:03 +0000

    RecipeCategory is Flags now

--- code/items/RecipeCategory.cs
+++ code/items/RecipeCategory.cs
@@ -1,10 +1,13 @@
-﻿namespace Facepunch.Forsaken;
+﻿using System;
 
+namespace Facepunch.Forsaken;
+
+[Flags]
 public enum RecipeCategory
 {
-	None,
-	Building,
-	Clothing,
-	Weapons,
-	Tools
+	None = 0,
+	Building = 1,
+	Clothing = 2,
+	Weapons = 4,
+	Tools = 8
 }
