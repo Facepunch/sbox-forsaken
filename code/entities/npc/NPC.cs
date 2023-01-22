eb564ba19c9897617bb5aa60d7cf65849bd65e45
kurozael <kurozael@gmail.com>
2023-01-22 23:08:10 +0000

    Add Max Idle Duration to NPC and keep properly grounded

--- code/entities/npc/NPC.cs
+++ code/entities/npc/NPC.cs
@@ -15,9 +15,14 @@ public partial class NPC : AnimatedEntity
 	[Property] public bool DoesWander { get; set; } = false;
 
 	/// <summary>
-	/// How long the NPC stays idle for before wandering again.
+	/// The minumum amount of time that the NPC will stay idle for before wandering again.
 	/// </summary>
-	[Property] public float IdleDuration { get; set; } = 30f;
+	[Property] public float MinIdleDuration { get; set; } = 30f;
+
+	/// <summary>
+	/// The maximum amount of time that the NPC will stay idle for before wandering again.
+	/// </summary>
+	[Property] public float MaxIdleDuration { get; set; } = 60f;
 
 	/// <summary>
 	/// The speed at which the NPC moves.
@@ -32,9 +37,9 @@ public partial class NPC : AnimatedEntity
 	{
 		if ( DoesWander && NextWanderTime && NavMesh.IsLoaded )
 		{
-			NextWanderTime = IdleDuration;
+			NextWanderTime = Game.Random.Float( MinIdleDuration, MaxIdleDuration );
 
-			var targetPosition = NavMesh.GetPointWithinRadius( Position, 100f, 2000f );
+			var targetPosition = NavMesh.GetPointWithinRadius( Position, 500f, 5000f );
 			if ( !targetPosition.HasValue ) return;
 
 			Path = NavMesh.PathBuilder( Position )
@@ -50,11 +55,16 @@ public partial class NPC : AnimatedEntity
 		GroundEntity = pm.Entity;
 
 		if ( !GroundEntity.IsValid() )
+		{
 			Velocity += Vector3.Down * 600f * Time.Delta;
+		}
 		else
+		{
+			Position = Position.WithZ( pm.EndPosition.z );
 			Velocity = Velocity.WithZ( 0f );
+		}
 
-		ApplyFriction( 6f );
+		ApplyFriction( 4f );
 		UpdatePathVelocity();
 		HandleAnimation();
 
@@ -120,7 +130,7 @@ public partial class NPC : AnimatedEntity
 	protected virtual BBox GetHull()
 	{
 		var girth = 16f;
-		var mins = new Vector3( -girth, -girth, 0 );
+		var mins = new Vector3( -girth, -girth, 0f );
 		var maxs = new Vector3( +girth, +girth, 72f );
 		return new BBox( mins, maxs );
 	}
