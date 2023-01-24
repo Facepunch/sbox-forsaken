b5dccda6cc31ce7274d598f2cfaec32e1f1791cc
kurozael <kurozael@gmail.com>
2023-01-24 23:43:27 +0000

    Don't show the same zone name too often

--- code/ui/Hud.Network.cs
+++ code/ui/Hud.Network.cs
@@ -4,10 +4,16 @@ namespace Facepunch.Forsaken.UI;
 
 public partial class Hud
 {
+	private static string LastZoneName { get; set; }
+	private static TimeSince LastZoneShown { get; set; }
+
 	[ClientRpc]
 	[ConCmd.Client]
     public static void ShowZoneName( string name )
     {
+		if ( LastZoneName == name && LastZoneShown < 10f )
+			return;
+
 		Current.ShowZoneName( name, 4f );
 	}
 }
