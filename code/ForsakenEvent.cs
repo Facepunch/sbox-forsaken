1a52aeb1a43574a263ec21d5c29ba4ede01552ca
kurozael <kurozael@gmail.com>
2023-02-15 11:45:39 +0000

    Added ForsakenEvent.NavBlockerAdded and ForsakenEvent.NavBlockerRemoved

---
+++ code/ForsakenEvent.cs
@@ -0,0 +1,22 @@
+﻿using Sandbox;
+
+namespace Facepunch.Forsaken;
+
+public static class ForsakenEvent
+{
+	public class NavBlockerAdded : EventAttribute
+	{
+		public NavBlockerAdded() : base( "fsk.navblocker.added" )
+		{
+
+		}
+	}
+
+	public class NavBlockerRemoved : EventAttribute
+	{
+		public NavBlockerRemoved() : base( "fsk.navblocker.removed" )
+		{
+
+		}
+	}
+}
