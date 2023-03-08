76fa5a9c0e7e1ddfc34fa6d342c093416ed8cbb0
kurozael <kurozael@gmail.com>
2023-03-08 12:36:18 +0000

    AvoidanceBehavior and WanderBehavior

---
+++ code/entities/npc/behavior/AvoidanceBehavior.cs
@@ -0,0 +1,104 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Facepunch.Forsaken;
+
+public class AvoidanceBehavior : EntityComponent
+{
+	public HashSet<string> ObstacleTags { get; set; } = new();
+
+	public float MaxAcceleration { get; set; } = 60f;
+	public float MinimumDistance { get; set; } = 80f;
+	public float MaxStandableAngle { get; set; } = 40f;
+
+	public float MainWhiskerLength { get; set; } = 90;
+	public float SideWhiskerLength { get; set; } = 50f;
+	public float SideWhiskerAngle { get; set; } = 45f;
+
+	private SteeringComponent Steering { get; set; }
+
+	protected override void OnActivate()
+	{
+		Steering = Entity.Components.GetOrCreate<SteeringComponent>();
+	}
+
+	public Vector3 GetSteering()
+	{
+		var velocityWithoutGravity = Entity.Velocity.WithZ( 0f );
+
+		if ( velocityWithoutGravity.Length > 0.005f )
+			return GetSteering( velocityWithoutGravity );
+		else
+			return GetSteering( Entity.Rotation.Forward );
+	}
+
+	public Vector3 GetSteering( Vector3 facingDir )
+	{
+		var acceleration = Vector3.Zero;
+		TraceResult trace;
+
+		if ( !FindObstacle( facingDir, out trace ) )
+		{
+			return acceleration;
+		}
+
+		var targetPostition = trace.HitPosition + Vector3.Reflect( trace.Direction, trace.Normal ) * MinimumDistance;
+		return Steering.Seek( targetPostition, MaxAcceleration );
+	}
+
+	private bool FindObstacle( Vector3 facingDir, out TraceResult result )
+	{
+		facingDir = facingDir.Normal;
+
+		var dirs = new Vector3[3];
+		dirs[0] = facingDir;
+
+		var orientation = SteeringComponent.VectorToOrientation( facingDir );
+		dirs[1] = SteeringComponent.OrientationToVector( orientation + SideWhiskerAngle.DegreeToRadian() );
+		dirs[2] = SteeringComponent.OrientationToVector( orientation - SideWhiskerAngle.DegreeToRadian() );
+
+		return CastWhiskers( dirs, out result );
+	}
+
+	private bool CastWhiskers( Vector3[] dirs, out TraceResult result )
+	{
+		result = default;
+
+		var obstacleTagsArray = ObstacleTags.ToArray();
+		var didHitAnything = false;
+		var origin = Entity.Position + Vector3.Up * 12f;
+
+		for ( int i = 0; i < dirs.Length; i++ )
+		{
+			var distance = i == 0 ? MainWhiskerLength : SideWhiskerLength;
+			var trace = Trace.Ray( origin, origin + dirs[i] * distance )
+				.WorldAndEntities()
+				.WithoutTags( "passplayers" )
+				.WithAnyTags( "solid" )
+				.WithAnyTags( obstacleTagsArray )
+				.Ignore( Entity )
+				.Size( 8f )
+				.Run();
+
+			if ( trace.Hit )
+			{
+				DebugOverlay.Line( origin, origin + dirs[i] * distance, Entity.Velocity.IsNearZeroLength ? Color.Cyan : Color.Green );
+
+				if ( trace.Normal.Angle( Vector3.Up ) > MaxStandableAngle )
+				{
+					didHitAnything = true;
+					result = trace;
+					break;
+				}
+			}
+			else
+			{
+				DebugOverlay.Line( origin, origin + dirs[i] * distance, Color.Red );
+			}
+		}
+
+		return didHitAnything;
+	}
+}
---
+++ code/entities/npc/behavior/WanderBehavior.cs
@@ -0,0 +1,38 @@
+﻿using Sandbox;
+using System;
+
+namespace Facepunch.Forsaken;
+
+public class WanderBehavior : EntityComponent
+{
+	public float WanderRadius { get; set; } = 256f;
+	public float WanderDistance { get; set; } = 24f;
+	public float WanderJitter { get; set; } = 16f;
+
+	private SteeringComponent Steering { get; set; }
+	private Vector3 CurrentTarget { get; set; }
+
+	protected override void OnActivate()
+	{
+		Steering = Entity.Components.GetOrCreate<SteeringComponent>();
+		Regenerate();
+	}
+
+	public void Regenerate()
+	{
+		var theta = Game.Random.Float() * 2f * MathF.PI;
+		CurrentTarget = new Vector3( WanderRadius * MathF.Cos( theta ), WanderRadius * MathF.Sin( theta ), 0f );
+	}
+
+	public Vector3 GetSteering()
+	{
+		var jitter = WanderJitter;
+		CurrentTarget += new Vector3( Game.Random.Float( -1f, 1f ) * jitter, Game.Random.Float( -1f, 1f ) * jitter, 0f );
+		CurrentTarget = CurrentTarget.Normal;
+		CurrentTarget *= WanderRadius;
+
+		var targetPosition = Entity.Position + Entity.Rotation.Forward * WanderDistance + CurrentTarget;
+
+		return Steering.Seek( targetPosition );
+	}
+}
