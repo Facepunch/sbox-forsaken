59bca2608d57b9be0af8b0a8d0a0d7623b542418
kurozael <kurozael@gmail.com>
2023-04-04 13:38:23 +0100

    Navigation debug overlay only shows nodes the camera is looking near

--- code/entities/npc/navigation/Navigation.Debug.cs
+++ code/entities/npc/navigation/Navigation.Debug.cs
@@ -10,10 +10,21 @@ public partial class Navigation
 		if ( Game.IsClient ) return;
 		if ( !IsReady ) return;
 
+		var trace = Trace.Ray( Camera.Main.Position, Camera.Main.Rotation.Forward * 10000f )
+			.WorldAndEntities()
+			.Run();
+
+		var distance = 300f * 300f;
+
 		for ( int x = 0; x < Grid.Length; x++ )
 		{
-			var color = Grid[x].Walkable ? Color.Green : Color.Red;
-			DebugOverlay.Sphere( ToWorld( x ).WithZ( Grid[x].ZOffset ), 1f, color, 0f );
+			var position = ToWorld( x ).WithZ( Grid[x].ZOffset );
+
+			if ( position.DistanceSquared( trace.EndPosition ) <= distance )
+			{
+				var color = Grid[x].Walkable ? Color.Green : Color.Red;
+				DebugOverlay.Sphere( position, 1f, color, 0f );
+			}
 		}
 	}
 }
