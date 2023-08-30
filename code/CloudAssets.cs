2c8cb5cb1f6acdad37a133f61bda4b189714cfc2
kurozael <kurozael@gmail.com>
2023-08-30 10:32:43 +0100

    Add CloudAssets file to store Cloud.Model calls

---
+++ code/CloudAssets.cs
@@ -0,0 +1,14 @@
+using Sandbox;
+using System.Collections.Generic;
+
+namespace Facepunch.Forsaken;
+
+public static class CloudAssets
+{
+	private static Model[] Models = new[]
+	{
+		Cloud.Model( "facepunch.wooden_crate" ),
+		Cloud.Model( "rust.fuel_barrel" ),
+		Cloud.Model( "facepunch.beech_bush_regular_medium_a" )
+	};
+}
