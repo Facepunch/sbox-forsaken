c8404e8be8a69bfc1126d0bdf1efb2ed5e00621f
kurozael <kurozael@gmail.com>
2023-08-30 10:32:51 +0100

    Fix plant fiber model path

--- code/entities/pickups/PlantFiberPickup.cs
+++ code/entities/pickups/PlantFiberPickup.cs
@@ -5,7 +5,7 @@ namespace Facepunch.Forsaken;
 public partial class PlantFiberPickup : ResourcePickup
 {
 	public override string GatherSound => "rummage.plant";
-	public override string ModelPath => "models/sbox_props/shrubs/pine/pine_bush_regular_c.vmdl";
+	public override string ModelPath => "models/sbox_props/shrubs/beech/beech_bush_regular_medium_a.vmdl";
 	public override Type ItemType => typeof( PlantFiberItem );
 	public override int StackSize => 15;
 
