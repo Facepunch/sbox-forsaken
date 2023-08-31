bf9b3d9d767bee825a10ffa8df28ebcd99ad8021
kurozael <kurozael@gmail.com>
2023-08-31 12:07:22 +0100

    Add watermelon to cloud asset so can use gib model..

--- code/CloudAssets.cs
+++ code/CloudAssets.cs
@@ -8,6 +8,7 @@ public static class CloudAssets
 	private static Model[] Models = new[]
 	{
 		Cloud.Model( "facepunch.wooden_crate" ),
+		Cloud.Model( "facepunch.watermelon" ),
 		Cloud.Model( "rust.fuel_barrel" ),
 		Cloud.Model( "facepunch.beech_bush_regular_medium_a" )
 	};
